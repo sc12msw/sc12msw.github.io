2af4ede510a3c327dd6a89bafb324788fc38eaaf
Matt Watson <matt.3.watson@morrisonsplc.co.uk>
2019-06-25 17:18:16 +0100

    Updated files

---
+++ js/main.js
@@ -0,0 +1,85 @@
+const pokaWrappers = [].slice.call(document.querySelectorAll(".poka-wrapper"));
+const messages =
+    const pokaWrappers = [].slice.call(document.querySelectorAll(".poka-wrapper"));
+const messages = [].slice.call(document.querySelectorAll(".title-wrapper > p"));
+
+const clearVisibleClassFromPokaWrappers = () => {
+    pokaWrappers.forEach(pokaWrapper => {
+        if (pokaWrapper.classList.contains("visible")) {
+            pokaWrapper.classList.remove("visible");
+        }
+    });
+};
+
+const addVisibleClassToPokaWrapper = index => {
+    console.log("index", index);
+    clearVisibleClassFromPokaWrappers();
+    if (!pokaWrappers[index].classList.contains("visible")) {
+        pokaWrappers[index].classList.add("visible");
+    }
+};
+
+const removeVisibleClassFromPokaWrapper = () => {
+    if (pokaWrapper.classList.contains("visible")) {
+        pokaWrapper.classList.remove("visible");
+    }
+};
+
+const checkAndPlayPokaDotAnimation = () => {
+    messages.forEach((message, index) => {
+        const bounding = message.getBoundingClientRect();
+        if (bounding.top >= 0 && bounding.top <= window.innerHeight) {
+            addVisibleClassToPokaWrapper(index);
+        }
+        //   else {
+        //   removeVisibleClassFromPokaWrapper();
+        // }
+    });
+};
+
+window.addEventListener("scroll", () => {
+    requestAnimationFrame(checkAndPlayPokaDotAnimation);
+});
+
+requestAnimationFrame(checkAndPlayPokaDotAnimation);
+[].slice.call(document.querySelectorAll(".title-wrapper > p"));
+
+const clearVisibleClassFromPokaWrappers = () => {
+    pokaWrappers.forEach((pokaWrapper) => {
+        if (pokaWrapper.classList.contains('visible')) {
+            pokaWrapper.classList.remove('visible')
+        }
+    });
+};
+
+const addVisibleClassToPokaWrapper = (index) => {
+    console.log('index', index);
+    clearVisibleClassFromPokaWrappers();
+    if (!pokaWrappers[index].classList.contains("visible")) {
+        pokaWrappers[index].classList.add("visible");
+    }
+};
+
+const removeVisibleClassFromPokaWrapper = () => {
+    if (pokaWrapper.classList.contains("visible")) {
+        pokaWrapper.classList.remove("visible");
+    }
+};
+
+const checkAndPlayPokaDotAnimation = () => {
+    messages.forEach((message, index) => {
+        const bounding = message.getBoundingClientRect();
+        if (bounding.top >= 0 && bounding.top <= window.innerHeight) {
+            addVisibleClassToPokaWrapper(index);
+        }
+        //   else {
+        //   removeVisibleClassFromPokaWrapper();
+        // }
+    });
+};
+
+window.addEventListener("scroll", () => {
+    requestAnimationFrame(checkAndPlayPokaDotAnimation);
+});
+
+requestAnimationFrame(checkAndPlayPokaDotAnimation);
