ecf7998366feb10b0791d01dd205557db73fa9b8
Matthew Watson <matthew.watson@skybettingandgaming.com>
2021-05-26 15:09:28 +0100

    Add help text and favicon

--- tojourn-app/src/components/header/index.tsx
+++ tojourn-app/src/components/header/index.tsx
@@ -3,7 +3,12 @@ import React from "react";
 export const Header: React.FC = () => {
     return (
         <div className="text-center" data-testid="container">
-            <h1 className="text-white text-xl m-4">{"ᛏᛟᛃᛟᚢᚱᚾ"}</h1>
+            <h1
+                title="Tojourn in Elder Futhark runic alphabet"
+                className="text-white text-xl m-4"
+            >
+                {"ᛏᛟᛃᛟᚢᚱᚾ"}
+            </h1>
         </div>
     );
 };
