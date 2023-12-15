684f11bcf69dfd9c3f06627334fc224049523eb6
Matt Watson <watson@tojourn.uk>
2023-12-15 18:49:53 +0000

    Add tenx job

--- tojourn-app/src/components/main/index.tsx
+++ tojourn-app/src/components/main/index.tsx
@@ -3,32 +3,33 @@ import React from "react";
 export const Main: React.FC = () => {
     return (
         <div>
-            <div className="sbg text-center font-light py-5 bg-white mb-8">
+            <div className="text-center font-light py-5 bg-purple mb-8">
                 <div className="bg-white p-5 mx-auto flex flex-col justify-center items-center">
                     <h3
                         data-test="main-heading"
-                        className="text-lg mb-8 max-w-xs"
+                        className="text-lg mb-8 max-w-xs font-bold"
                     >
-                        Currently employed at Sky Betting & Gaming as a Senior
-                        Software Engineer
+                        Currently employed at 10x Banking
+                        Senior Software Engineer
                     </h3>
                     <a
-                        href="https://www.skybetcareers.com/"
+                        href="https://www.10xbanking.com/community/10x-spotlight-matt-watson-software-engineer"
                         className="mb-8"
                         target="_blank"
                     >
                         {" "}
                         <img
-                            src="/img/SBG-Logo.png"
+                            src="/img/tenx.svg"
+
                             className="max-w-xs mb-8"
                         />
-                        Come join us!
+                        Read my article featured on 10x's website!
                     </a>
                     <p className="text-m mb-3 max-w-xs">
-                        Part of the Gaming Data Streaming squad working with
-                        Kotlin / Kafka / Kubernetes / Typescript / Chef / AWS /
-                        Docker / Jenkins / Jira / Confluence / Git (bitbucket)
-                        and lots of other technologies!
+                    Part of a SCA (Strong Customer Authentication) and Oauth Token management feature team. 
+                    Providing high risk login flows and JWT manament to over 2 million customers with 99.9999% (four nines) availability. 
+                    We used Java 17 Springboot microservices with Postgres (Aurora), Kafka and Redis (Elasticache) on AWS EKS for Kubernetes. 
+                    CI/CD using Jenkins and Bitbucket.
                     </p>
                 </div>
             </div>
