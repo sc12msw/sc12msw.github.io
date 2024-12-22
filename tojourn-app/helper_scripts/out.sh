ac64e45be93de7aef36ade0bc0307032c1942700
Matt Watson <watson@tojourn.uk>
2024-12-22 13:36:07 +0000

    Star wars timez

--- tojourn-app/helper_scripts/out.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash
-echo "Moving files to root of project due to laziness..."
-rm -r ../_next
-cp -r out/* ../
-rm -r out/
--- tojourn-app/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { Header } from "./header";
-export { Main } from "./main";
-export { Button } from "./button";
-export { Cards } from "./cards";
-export { Footer } from "./footer";
-export { Container } from "./container";
-export { Profile } from "./profile";
-export { Skills } from "./skills";
